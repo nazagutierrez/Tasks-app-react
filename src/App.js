0e62952f4d158c7066084c7083ec0f94aa5057e1
nazagutierrez <nazarenojunin@gmail.com>
2022-10-03 15:51:50 -0300

    Cambios en el README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.scss"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import TaskForm from "./components/taskForm";
+import TaskList from "./components/taskList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/create-task" element={<TaskForm />} />
+          <Route path="/edit-task/:id" element={<TaskForm />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
