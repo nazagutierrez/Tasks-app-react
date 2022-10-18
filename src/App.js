8b8b2f99d91f9c07282b4ea9dbf626663d96ec98
nazagutierrez <nazarenojunin@gmail.com>
2022-10-18 18:37:07 -0300

    try to fix other time

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles/App.scss"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 
 import TaskForm from "./components/taskForm";
 import TaskList from "./components/taskList";
@@ -7,13 +7,13 @@ import TaskList from "./components/taskList";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<TaskList />} />
           <Route path="/create-task" element={<TaskForm />} />
           <Route path="/edit-task/:id" element={<TaskForm />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
